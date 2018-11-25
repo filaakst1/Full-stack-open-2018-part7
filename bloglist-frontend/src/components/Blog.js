b5b3b35609738a7a18cba1f4b69d505353ac912b
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-25 11:46:22 +0200

    Blog now renders the details of single blog

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -8,43 +8,14 @@ class Blog extends React.Component {
     }
   }
   render() {
-    const { blog, like, deletable, remove } = this.props
-
-    const blogStyle = {
-      paddingTop: 10,
-      paddingLeft: 2,
-      border: 'solid',
-      borderWidth: 1,
-      marginBottom: 5
-    }
-
-    const contentStyle = {
-      display: this.state.visible? '' : 'none',
-      margin: 5,
-    }
-
+    const { blog, like } = this.props
     const adder = blog.user ? blog.user.name : 'anonymous'
-
     return (
-      <div style={blogStyle}>
-        <div
-          onClick={() => this.setState({ visible: !this.state.visible })}
-          className='name'
-        >
-          {blog.title} {blog.author}
-        </div>
-        <div style={contentStyle} className='content'>
-          <div>
-            <a href={blog.url}>{blog.url}</a>
-          </div>
-          <div>
-            {blog.likes} likes <button onClick={like}>like</button>
-          </div>
-          <div>
-            added by {adder}
-          </div>
-          {deletable && <div><button onClick={remove}>delete</button></div>}
-        </div>
+      <div>
+        <h2>{blog.title} {blog.author}</h2>
+        <a href={blog.url}>{blog.url}</a>
+        <div>{blog.likes} likes <button onClick={like}>like</button></div>
+        <div>added by {adder}</div>
       </div>
     )
   }
