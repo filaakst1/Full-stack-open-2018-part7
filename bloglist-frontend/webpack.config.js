1719406ea992bfea79e3d0ddf5b74fecd3d315a2
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-12-01 18:19:20 +0200

    Webpack mostly configured. Still needs proxy

--- bloglist-frontend/webpack.config.js
+++ bloglist-frontend/webpack.config.js
@@ -1,10 +1,40 @@
 const path = require('path')
 
 const config = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill','./src/index.js'],
   output: {
     path: path.resolve( __dirname, 'dist'),
     filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/react'
+            ],
+            plugins: [
+              require('babel-plugin-transform-class-properties'),
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      }
+    ]
   }
+
 }
 module.exports = config
